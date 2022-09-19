88165a803a6ab29686a8f0a9107023966a880971
taua <taua.a@icloud.com>
2022-09-19 07:48:31 -0300

    feat: add user model, migration, factory

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,29 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
+import Database from '@ioc:Adonis/Lucid/Database'
+import User from 'App/Models/User'
 
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.get('/users', async () => {
+  return await Database.from('users').select('*')
+})
+
+Route.post('/users', async ({ request, response }) => {
+  const user = new User()
+  user.username = request.input('username')
+  user.email = request.input('email')
+  user.password = request.input('password')
+  user.avatarUrl = request.input('avatarUrl')
+  await user.save()
+
+  return response.status(201).json(user.$isPersisted)
+})
+Route.put('/users', async () => {
+  return {}
+})
+Route.delete('/users', async () => {
+  return {}
 })
